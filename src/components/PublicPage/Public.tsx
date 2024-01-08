2afa86d33d27e11a02010fe95d8688c922467bcf
burshilya2024 <bursh2024@gmail.com>
2024-01-08 15:00:19 +0300

    TEST:BURSH_PUBLICK

--- src/components/PublicPage/Public.tsx
+++ src/components/PublicPage/Public.tsx
@@ -13,6 +13,7 @@ export default function Public() {
 
   useEffect(() => {
     const fetchPhotos = async () => {
+      //! ТЕСТОВОЕ API ДЛЯ ОТОБРОЖЕНИЕ MOCK FOTO, publick_bursh
       try {
         const response = await axios.get(
           `https://api.unsplash.com/photos?client_id=${API_KEY}&per_page=8&page=1`
