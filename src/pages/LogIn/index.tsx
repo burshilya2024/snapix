5c6f91193fbf054cccc1c8ac5eb5fca2d01f8eab
burshilya2024 <bursh2024@gmail.com>
2024-01-08 16:17:40 +0300

    fix:type

--- src/pages/LogIn/index.tsx
+++ src/pages/LogIn/index.tsx
@@ -1,54 +1,55 @@
-import Form from "@/components/Form/Form";
-import React, { useState } from 'react';
+import React, { useState } from 'react'
+
+import Button from '@/components/Button/Button'
+import Form from '@/components/Form/Form'
+
 import styles from '@/styles/LogIn.module.scss'
-import Button from "@/components/Button/Button";
 
 const LogIn: React.FC = () => {
-    const [email, setEmail] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
-    const [isSubmitting, setIsSubmitting] = useState<boolean>(false)
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false)
 
-    const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        setIsSubmitting(true)
-        await new Promise((resolve)=> setTimeout(resolve, 1000))
+  const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    setIsSubmitting(true)
+    await new Promise(resolve => setTimeout(resolve, 1000))
 
-        setEmail('')
-        setPassword('')
-        setIsSubmitting(false)
-    }
-    return (
-        <Form onSubmit={handleLogin}>
-            <div className={styles.tittle}>Sign In</div>
-            <div className={styles.inputGroup}>
-                <input
-                    type="email"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    className={styles.inputField}
-                    required
-                />
-            </div>
-            <div className={styles.inputGroup}>
-                <input
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    className={styles.inputField}
-                    required
-                />
-                {/*TODO: make "input" be an single component*/}
-            </div>
-            <div>
-                Forgot Password
-            </div>
-            <Button type="submit">Sign in</Button>
-            <div> Don't have an account?</div>
-            <Button outline>Sign up</Button>
-        </Form>
-    );
-};
+    setEmail('')
+    setPassword('')
+    setIsSubmitting(false)
+  }
 
-export default LogIn;
+  return (
+    <Form onSubmit={handleLogin}>
+      <div className={styles.tittle}>Sign In</div>
+      <div className={styles.inputGroup}>
+        <input
+          className={styles.inputField}
+          onChange={e => setEmail(e.target.value)}
+          placeholder={'Email'}
+          required
+          type={'email'}
+          value={email}
+        />
+      </div>
+      <div className={styles.inputGroup}>
+        <input
+          className={styles.inputField}
+          onChange={e => setPassword(e.target.value)}
+          placeholder={'Password'}
+          required
+          type={'password'}
+          value={password}
+        />
+        {/*TODO: make "input" be an single component*/}
+      </div>
+      <div>Forgot Password</div>
+      <Button primary>Sign in</Button>
+      <div> Dont have an account?</div>
+      <Button outline>Sign up</Button>
+    </Form>
+  )
+}
+
+export default LogIn
