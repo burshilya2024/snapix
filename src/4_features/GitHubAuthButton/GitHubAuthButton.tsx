4b4581aa5e0bca51d18231c7aae02748c751587a
Siltrim <rojerr410@gmail.com>
2024-02-11 00:11:01 +0600

    try fix vercel

---
+++ src/4_features/GitHubAuthButton/GitHubAuthButton.tsx
@@ -0,0 +1,11 @@
+import GitHubIcons from '@public/assets/icons/github.svg'
+
+import styles from '@/styles/LogIn.module.scss'
+
+export const GitHubAuthButton = () => {
+  return (
+    <div className={styles.gitHubAthIcon}>
+      <GitHubIcons />
+    </div>
+  )
+}
