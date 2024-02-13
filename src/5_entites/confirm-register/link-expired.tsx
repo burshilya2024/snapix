3efa551e528e5070eb3a0afd14f8c954bbcc3c3c
AlexAbrello <abrello063@gmail.com>
2024-02-13 15:26:27 +0300

    added link-expired.tsx

--- src/5_entites/confirm-register/link-expired.tsx
+++ src/5_entites/confirm-register/link-expired.tsx
@@ -3,27 +3,26 @@ import React from 'react'
 import { useTranslation } from '@/6_shared/config/i18n/hook/useTranslation'
 import Button from '@/6_shared/ui/ui-button'
 import { Heading } from '@chakra-ui/react'
-import img from '@public/assets/images/confirmed.png'
+import img from '@public/assets/images/rafiki.png'
 import Image from 'next/image'
-import Link from 'next/link'
 
 import styles from '@/styles/Confirm.module.scss'
 
-export function ConfirmPasswordComponent() {
+export function LinkExpiredComponent() {
   const { t } = useTranslation()
 
   return (
     <div className={styles.container}>
       <div className={styles.textContainer}>
-        <Heading size={'xl'}>Congratulations!</Heading>
+        <Heading size={'xl'}>Email verification link expired!</Heading>
       </div>
       <div className={styles.textContainer}>
-        <Heading size={'lg'}>Your email has been confirmed</Heading>
+        <Heading size={'lg'}>
+          Looks like the verification link has expired. Not to worry, we can send the link again
+        </Heading>
       </div>
       <div className={styles.buttonContainer}>
-        <Link href={'/LogIn'}>
-          <Button primary>{t.signIn_SignUp.signIn}</Button>
-        </Link>
+        <Button primary>Resend verification link</Button>
       </div>
       <div className={styles.textContainer}>
         <Image alt={''} src={img} />
